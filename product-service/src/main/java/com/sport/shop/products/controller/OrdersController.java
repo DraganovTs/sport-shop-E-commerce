497290318f2c9a4c06d0eb3a6212c448c23a88f5
DraganovTs <cvetomir_draganov@abv.bg>
2024-06-16 21:59:43 +0300

    Oauth2 jwt token

--- product-service/src/main/java/com/sport/shop/products/controller/OrdersController.java
+++ product-service/src/main/java/com/sport/shop/products/controller/OrdersController.java
@@ -1,6 +1,8 @@
 package com.sport.shop.products.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +17,10 @@ public class OrdersController {
     public List<String> getOrders(){
         String[] orders = {"Order1","Order2","Order3"};
         return Arrays.asList(orders);
+    }
+
+    @PostMapping("v1/sport/orderes")
+    public ResponseEntity<?> putOrder(){
+        return null;
     }
 }
